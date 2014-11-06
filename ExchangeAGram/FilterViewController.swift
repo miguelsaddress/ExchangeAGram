b3a1c15de9d05d4fab90b87836b06cb3cc806f44
Miguel A. Moreno <miguel@tyba.com>
2014-11-06 12:51:20 +0100

    Adding FilterViewController

---
+++ ExchangeAGram/FilterViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FilterViewController.swift
+//  ExchangeAGram
+//
+//  Created by Miguel Angel Moreno Armenteros on 06/11/14.
+//  Copyright (c) 2014 Miguel Angel Moreno Armenteros. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    var thisFeedItem: FeedItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
