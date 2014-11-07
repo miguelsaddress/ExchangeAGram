0adc391fb1e1c6df56c9a82133a059ea2db602ec
Miguel A. Moreno <miguel@tyba.com>
2014-11-07 13:21:02 +0100

    Adding ProfileViewController

---
+++ ExchangeAGram/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  ExchangeAGram
+//
+//  Created by Miguel Angel Moreno Armenteros on 07/11/14.
+//  Copyright (c) 2014 Miguel Angel Moreno Armenteros. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
