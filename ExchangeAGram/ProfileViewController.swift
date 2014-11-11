f7ade01cb6beb58cd4ae6c927b134af732dbb667
Miguel A. Moreno <miguel@tyba.com>
2014-11-11 17:52:49 +0100

    Conforming to Facebook FBLoginViewDelegate

--- ExchangeAGram/ProfileViewController.swift
+++ ExchangeAGram/ProfileViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, FBLoginViewDelegate {
 
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var fbLoginView: FBLoginView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +25,21 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
+        //
+    }
+    
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        //
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
+        //
+    }
+    
+    func loginView(loginView: FBLoginView!, handleError error: NSError!) {
+        
+    }
 
 }
